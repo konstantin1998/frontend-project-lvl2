98c9f1ef354f18f5e7c77fec48f58b23c90ab5dd
konstantin1998 <galperin.15@yandex.ru>
2019-08-24 13:42:00 +0300

    fix syntax mistakes

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -4,26 +4,26 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const dirName = __dirname;
-const filePaths = [ `${dirName}/__fixtures__/before.json`,
-                    `${dirName}/__fixtures__/after.json`,
-                    `${dirName}/__fixtures__/jsonDiff`,
+const filePaths = [`${dirName}/__fixtures__/before.json`,
+  `${dirName}/__fixtures__/after.json`,
+  `${dirName}/__fixtures__/jsonDiff`,
 
-                    `${dirName}/__fixtures__/before.yml`,
-                    `${dirName}/__fixtures__/after.yml`,
-                    `${dirName}/__fixtures__/ymlDiff`,
+  `${dirName}/__fixtures__/before.yml`,
+  `${dirName}/__fixtures__/after.yml`,
+  `${dirName}/__fixtures__/ymlDiff`,
 
-                    `${dirName}/__fixtures__/before.ini`,
-                    `${dirName}/__fixtures__/after.ini`,
-                    `${dirName}/__fixtures__/iniDiff`];
+  `${dirName}/__fixtures__/before.ini`,
+  `${dirName}/__fixtures__/after.ini`,
+  `${dirName}/__fixtures__/iniDiff`];
 const chunkSize = 3;
 const ArraysForTest = _.chunk(filePaths, chunkSize);
 test.each(ArraysForTest)('compareTest', (fileBeforePath, fileAfterPath, rightAnswerPath) => {
-                                          const right = fs.readFileSync(rightAnswerPath).toString().trim();
-                                          const actual = comparator(fileBeforePath, fileAfterPath).trim();
+  const right = fs.readFileSync(rightAnswerPath).toString().trim();
+  const actual = comparator(fileBeforePath, fileAfterPath).trim();
 
-                                          expect(actual).toEqual(right);
-                                          });
-/*test('jsonCompare', () => {
+  expect(actual).toEqual(right);
+});
+/* test('jsonCompare', () => {
   const fileBeforePath = `${dirName}/__fixtures__/before.json`;
   const fileAfterPath = `${dirName}/__fixtures__/after.json`;
   const rightAnswerPath = `${dirName}/__fixtures__/jsonDiff`;
@@ -41,4 +41,4 @@ test('yamlCompare', () => {
   const actual = comparator(fileBeforePath, fileAfterPath).trim();
 
   expect(actual).toEqual(right);
-});*/
+}); */
